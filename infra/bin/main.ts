d07dbd2991e822c2a9803961e238fa2eff4bacd5
clayton <claytonhthompson@gmail.com>
2024-01-06 10:02:41 -0800

    Lint fixes and import/export cleanup

--- infra/bin/main.ts
+++ infra/bin/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import UserManagementStack from "../lib/user-managment-stack";
+import UserManagementStack from "../lib";
 
 const config = {
   accountId: "022703707499",
