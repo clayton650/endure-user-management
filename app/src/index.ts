08728d4799ad8f74b706e2bc92f07441c24e5db4
clayton <claytonhthompson@gmail.com>
2024-01-14 00:31:16 -0800

    Change endpoint for testing purposes

--- app/src/index.ts
+++ app/src/index.ts
@@ -5,7 +5,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from user management's new lambda! And Again",
+      message:
+        "Hello from user management's new lambda! Checking if the pipeline works!",
     }),
   };
 };
