7deb3ca54336b0fe1a48b2c50d9277dec767c40e
clayton <claytonhthompson@gmail.com>
2024-02-18 16:41:51 -0800

    Clean up auth function in prep to convert it to get user details

--- app/src/auth/auth.ts
+++ app/src/auth/auth.ts
@@ -1,12 +1,9 @@
-import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
-import initBaseAuth from "../cognito/propelauth";
 import SecretManagerError from "./SecretManagerError";
 import UnauthorizedUserError from "./UnauthorizedUserError";
 import UserPoolClientError from "../cognito/UserPoolClientError";
 import InternalServerError from "./InternalServerError";
 
 type AccessToken = string;
-type UserId = string;
 
 // TODO: move to common types
 interface User {
@@ -24,35 +21,17 @@ interface UserAuthInfo {
   user: User;
 }
 
-export default async function auth(
-  accessToken: AccessToken,
-  userId: UserId,
-): Promise<UserAuthInfo> {
+export default async function auth(): Promise<UserAuthInfo> {
   try {
-    // TODO: move PropelAuth validation to  to a separate lambda triggered by Cognito DefineAuthChallenge
-    const { validateAccessTokenAndGetUser } = await initBaseAuth();
-    const authHeader = `Bearer ${accessToken}`;
-    const propelUser = await validateAccessTokenAndGetUser(authHeader);
-
-    // TODO: is a userId check unnecessary? Do we even need to pass the userId?
-    if (userId !== propelUser.userId) {
-      throw new UnauthorizedUserError("Not the expected user");
-    }
-
-    // TODO: has paid bill (subscription level)
-
-    const userMobile = "1-555-555-5555";
-
-    // TODO: move user related info to another endpoint/function?
     const user = {
-      id: propelUser.userId,
-      email: propelUser.email,
-      firstName: propelUser.firstName!,
-      lastName: propelUser.lastName!,
-      mobile: userMobile,
-      pictureUrl: propelUser.metadata?.pictureUrl,
+      id: "fake id",
+      email: "fake email",
+      firstName: "fake name",
+      lastName: "fake password",
+      mobile: "fake mobile",
+      pictureUrl: "fake picture",
       humanVerified: true,
-      lastSeen: propelUser.metadata?.lastSeen?.toString(),
+      lastSeen: "fake date",
     };
 
     return {
