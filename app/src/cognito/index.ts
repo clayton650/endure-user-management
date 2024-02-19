b5a34883d096ba17d9258fc3cf582824f1e3dbaf
clayton <claytonhthompson@gmail.com>
2024-02-18 16:49:06 -0800

    Remove createAuthChallengeHandler

--- app/src/cognito/index.ts
+++ app/src/cognito/index.ts
@@ -1,9 +1,4 @@
-import createAuthChallengeHandler from "./delete-create-auth-challenge";
 import defineAuthChallengeHandler from "./define-auth-challenge";
 import verifyAuthChallengeResponseHandler from "./verify-auth-challenge-response";
 
-export {
-  createAuthChallengeHandler,
-  defineAuthChallengeHandler,
-  verifyAuthChallengeResponseHandler,
-};
+export { defineAuthChallengeHandler, verifyAuthChallengeResponseHandler };
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,11 @@
 import authHandler from "./auth/lambda";
 import {
-  createAuthChallengeHandler,
   defineAuthChallengeHandler,
   verifyAuthChallengeResponseHandler,
 } from "./cognito";
 
 export {
   authHandler,
-  createAuthChallengeHandler,
   defineAuthChallengeHandler,
   verifyAuthChallengeResponseHandler,
 };
