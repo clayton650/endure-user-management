064cc9339f64a933baf8c5acc08b7ef5a9fe378d
clayton <claytonhthompson@gmail.com>
2024-03-10 11:41:48 -0700

    Add type

---
+++ infra/config/type.d.ts
@@ -0,0 +1,10 @@
+export default interface Config {
+  accountId: string;
+  region: string;
+  project: string;
+  envName: string;
+  repo: string;
+  branch: string;
+  domainName: string;
+  subDomain: string;
+}
